ff00a60470aec06b441f73e3032455149ddd5bd3
황종인 <ghkdwhddls12@daum.net>
2023-10-20 12:47:03 +0900

    스택형 내비게이션 설정

--- frontend/zip-app/App.js
+++ frontend/zip-app/App.js
@@ -1,12 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
+// Android 또는 iOS용으로 빌드하는 경우 프로덕션 단계에서 앱이 중단될 수 있기 때문에 무조건 넣어줘야함.
+import 'react-native-gesture-handler';
+
+// import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+// 화면이동
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './pages/HomeScreen'
+import CalendarScreen from './pages/CalendarScreen'
+import ScheduleScreen from './pages/ScheduleScreen'
+import DiaryScreen from './pages/DiaryScreen'
+import MypageScreen from './pages/MypageScreen'
+
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="홈" component={HomeScreen} />
+        <Stack.Screen name="캘린더" component={CalendarScreen} />
+        <Stack.Screen name="일정" component={ScheduleScreen} />
+        <Stack.Screen name="일기" component={DiaryScreen} />
+        <Stack.Screen name="프로필" component={MypageScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>
   );
 }
 
