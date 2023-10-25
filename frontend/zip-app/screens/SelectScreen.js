e2155acd8308f2cfb99a20f75509cfdaa6b74c37
이형석 <gudtjr2949@naver.com>
2023-10-25 13:51:02 +0900

    feat: 로그인 AsyncStorage AccessToken 저장 기능 추가

--- frontend/zip-app/screens/SelectScreen.js
+++ frontend/zip-app/screens/SelectScreen.js
@@ -1,15 +1,29 @@
 import { View, Text, Button } from 'react-native';
+import { useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function SelectScreen({ navigation }) {
-	return (
-		<View>
-			<Text>가족 선택 화면</Text>
-			<Text>가족 생성</Text>
-			<Text>가족 등록</Text>
-			<Button
-				title="가족 페이지로 이동"
-				onPress={() => navigation.navigate('홈')}
-			/>
-		</View>
-	);
+  
+  useEffect(() => {
+    // 비동기 함수를 정의
+    const fetchAccessToken = async () => {
+      const accessToken = await AsyncStorage.getItem('accessToken');
+      console.log("localstorage에서 가져온 토큰 : ", accessToken);
+    };
+
+    // 비동기 함수 호출
+    fetchAccessToken();
+  }, []); // 빈 dependency 배열로 한 번만 실행되게 함
+
+  return (
+    <View>
+      <Text>가족 선택 화면</Text>
+      <Text>가족 생성</Text>
+      <Text>가족 등록</Text>
+      <Button
+        title="가족 페이지로 이동"
+        onPress={() => navigation.navigate('홈')}
+      />
+    </View>
+  );
 }
--- frontend/zip-app/screens/auth/KakaoLoginScreen.js
+++ frontend/zip-app/screens/auth/KakaoLoginScreen.js
@@ -1,60 +1,60 @@
-import {WebView} from 'react-native-webview';
+import { WebView } from 'react-native-webview';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { StyleSheet, View, Text, Button } from 'react-native';
-import axios from "axios"
+import axios from 'axios';
 
-const REST_API_KEY = ''
+const REST_API_KEY = '59355f756d67cb7ecec20ede9b74ec8c';
 // const REDIRECT_URI = 'https://auth.expo.io/@hyeongseoklee/zip-app'
-const REDIRECT_URI = 'http://192.168.0.8:8081/auth/kakao/callback'
+// const REDIRECT_URI = 'http://172.20.10.3:8081/auth/kakao/callback';
+const REDIRECT_URI = 'http://192.168.31.236:8081/auth/kakao/callback';
 
 const INJECTED_JAVASCRIPT = `window.ReactNativeWebView.postMessage('message from webView')`;
 
 export default function KakaoLoginScreen({ navigation }) {
-  const getCode = (target) => {
-    const exp = 'code=';
-    const condition = target.indexOf(exp);
-    if (condition !== -1) {
-      const requestCode = target.substring(condition + exp.length);
-      console.log("code = ", requestCode);
-      requestToken(requestCode);
-    }
-  };
+	const getCode = (target) => {
+		const exp = 'code=';
+		const condition = target.indexOf(exp);
+		if (condition !== -1) {
+			const requestCode = target.substring(condition + exp.length);
+			console.log('code = ', requestCode);
+			requestToken(requestCode);
+		}
+	};
 
-  const requestToken = async (code) => {
-    const requestTokenUrl = 'http://localhost:9090/api/auth/kakao';
-  
-    try {
-      const body = {
-        code,
-      };
-      const response = await axios.post(requestTokenUrl, body);
-      console.log(response.data);
-      // const value = response.data;
-      // const result = await storeUser(value);
-      // if (result === 'stored') {
-      //   const user = await getData('user');
-      //   dispatch(read_S(user));
-      //   await navigation.navigate('Main');
-      // }
-      await navigation.navigate('가족선택');
-    } catch (e) {
-      console.log(e);
-    }
-  };
+	const requestToken = async (code) => {
+		const requestTokenUrl = 'http://10.0.2.2:9090/api/auth/kakao';
+    // const requestTokenUrl = 'http://172.20.10.3:9090/api/auth/kakao';
 
-  return (
-    <View style={{ flex: 1 }}>
-      <WebView
-        style={{ flex: 1 }}
-        source={{
-          uri: `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}`,
-        }}
-        injectedJavaScript={INJECTED_JAVASCRIPT}
-        javaScriptEnabled
-        onMessage={event => {
-          const data = event.nativeEvent.url;
-          getCode(data);
-        }}
-      />
-    </View>
-  );
+		try {
+			const body = {
+				code,
+			};
+			const response = await axios.post(requestTokenUrl, body);
+			console.log(response.data);
+			// accessToken을 AsyncStorage에 저장
+			if (response.data && response.data.accessToken) {
+				await AsyncStorage.setItem('accessToken', response.data.accessToken);
+			}
+			await navigation.navigate('가족선택');
+		} catch (e) {
+			console.log(e);
+		}
+	};
+
+	return (
+		<View style={{ flex: 1 }}>
+			<WebView
+				style={{ flex: 1 }}
+				source={{
+					uri: `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}`,
+				}}
+				injectedJavaScript={INJECTED_JAVASCRIPT}
+				javaScriptEnabled
+				onMessage={(event) => {
+					const data = event.nativeEvent.url;
+					getCode(data);
+				}}
+			/>
+		</View>
+	);
 }
