a3949e5c1c2ed86972fd1afbb37f67b04c5b0603
이형석 <gudtjr2949@naver.com>
2023-10-25 15:52:48 +0900

    feat: 메인화면 일정 리스트 조회

--- frontend/zip-app/screens/FamilyMainScreen.js
+++ frontend/zip-app/screens/FamilyMainScreen.js
@@ -1,27 +1,68 @@
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, ImageBackground, FlatList } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function FamilyMainScreen() {
-	return (
-		<View style={styles.container}>
-			<View style={styles.family}>
-				<Text>가족 메인 화면</Text>
-			</View>
-		</View>
-	);
+    const [schedules, setSchedules] = useState([]);
+
+    // useEffect(() => {
+    //     async function fetchData() {
+    //         const familyId = await AsyncStorage.getItem('familyId');
+    //         const response = await fetch(`http://10.0.2.2:9090/api/schedule/list?familyId=${familyId}`, {
+    //             method: 'GET',
+    //             headers: {
+    //                 'Accept': 'application/json',
+    //                 'Content-Type': 'application/json',
+    //             }
+    //         });
+
+    //         const data = await response.json();
+    //         setSchedules(data);
+    //     }
+
+    //     fetchData();
+    // }, []);
+
+    return (
+        <ImageBackground
+            source={require('../assets/family.png')}
+            style={styles.container}
+            resizeMode="cover"
+        >
+            <Text style={styles.title}>가족 메인 화면</Text>
+            <FlatList 
+                data={schedules}
+                renderItem={({ item }) => (
+                    <View style={styles.scheduleItem}>
+                        <Text>{item.name}</Text>
+                        <Text>{item.startDate} - {item.endDate}</Text>
+                    </View>
+                )}
+                keyExtractor={item => item.scheduleId.toString()}
+            />
+        </ImageBackground>
+    );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-	family: {
-		borderWidth: 1,
-		borderColor: 'black',
-		alignItems: 'center',
-		justifyContent: 'center',
-		width: 300,
-		height: 400,
-	},
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 20,
+    },
+    scheduleItem: {
+        borderWidth: 1,
+        borderColor: 'white',
+        borderRadius: 10,
+        padding: 10,
+        marginVertical: 5,
+        width: '90%',
+        alignItems: 'center',
+        backgroundColor: 'rgba(255,255,255,0.5)',
+    },
 });
