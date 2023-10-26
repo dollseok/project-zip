1fe5e3787c780f536053ba3a0f439ae24b8805f4
이형석 <gudtjr2949@naver.com>
2023-10-26 17:33:52 +0900

    feat: 가족 관련 수정버튼 추가

--- frontend/zip-app/screens/FamilyMainScreen.js
+++ frontend/zip-app/screens/FamilyMainScreen.js
@@ -6,6 +6,8 @@ import {
 	ImageBackground,
 	FlatList,
 	Image,
+	TouchableOpacity,
+	TextInput,
 } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import axiosInstance from '../util/Interceptor';
@@ -14,6 +16,23 @@ export default function FamilyMainScreen() {
 	const [family, setFamily] = useState([]);
 	const [schedules, setSchedules] = useState([]);
 	const [diaries, setDiaries] = useState([]);
+	const [isEditMode, setIsEditMode] = useState(false); // 편집 모드 상태 추가
+	
+
+	// 편집 모드를 토글하는 함수
+	const toggleEditMode = () => {
+		setIsEditMode((prevMode) => !prevMode);
+	};
+
+	const handleEdit = () => {
+		setIsEditMode(true);
+	  };
+	
+
+	const handleSave = () => {
+		setFamily({ ...family, familyName: newFamilyName });
+		setIsEditMode(false);
+	  };
 
 	useEffect(() => {
 		async function fetchData() {
@@ -51,13 +70,46 @@ export default function FamilyMainScreen() {
 
 	return (
 		<ImageBackground
-			source={{ uri: family.familyProfileImgUrl }}
+			// source={{ uri: family.familyProfileImgUrl }}
 			style={styles.container}
 			resizeMode="cover"
 		>
-			<Text style={styles.familyName}>{family.familyName}</Text>
+			<View style={styles.header}>
+				<TouchableOpacity onPress={() => setIsEditMode(!isEditMode)}>
+					<Image
+						source={require('../assets/geer.png')}
+						style={styles.editButton}
+					/>
+				</TouchableOpacity>
+			</View>
 
-			<Text style={styles.familyContent}>{family.familyContent}</Text>
+			<View style={styles.familyTextContainer}>
+				<Text style={styles.familyName}>{family.familyName}</Text>
+				{isEditMode && (
+					<TouchableOpacity
+						style={styles.editButtonFamilyText}
+						onPress={() => {}}
+					>
+						<Image
+							source={require('../assets/pencil.png')}
+							style={styles.editButtonIcon}
+						/>
+					</TouchableOpacity>
+				)}
+
+				<Text style={styles.familyContent}>{family.familyContent}</Text>
+				{isEditMode && (
+					<TouchableOpacity
+						style={styles.editButtonFamilyText}
+						onPress={() => {}}
+					>
+						<Image
+							source={require('../assets/pencil.png')}
+							style={styles.editButtonIcon}
+						/>
+					</TouchableOpacity>
+				)}
+			</View>
 
 			<Image
 				source={{ uri: family.memberProfileImgUrl }}
@@ -104,10 +156,29 @@ const styles = StyleSheet.create({
 		flex: 1,
 		alignItems: 'center',
 		justifyContent: 'center',
+		backgroundColor: 'black'
+	},
+	header: {
+		width: '100%',
+		flexDirection: 'row',
+		justifyContent: 'flex-end', // 이 부분 변경
+		paddingHorizontal: 20,
+		paddingTop: 30,
+	},
+	editButton: {
+		width: 30,
+		height: 30,
+	},
+	familyTextContainer: {
+		flexDirection: 'row', // 아이템을 가로 방향으로 배열
+		alignItems: 'center', 
+		justifyContent: 'center', // 아이템들 사이의 간격을 최대로 설정
+		width: '100%', // 컨테이너의 너비를 최대로 설정하여 오른쪽 끝에 연필 아이콘이 위치하도록 함
+		paddingHorizontal: 10, // 좌우 패딩을 설정하여 아이템들이 가장자리에 닿지 않게 함
 	},
 	familyName: {
 		fontSize: 30,
-		marginTop: 50,
+		marginTop: 20,
 		fontWeight: 'bold',
 		color: 'white',
 	},
@@ -163,5 +234,14 @@ const styles = StyleSheet.create({
 		width: '90',
 		alignItems: 'center',
 		backgroundColor: 'rgba(255,255,255,0.5)',
+	},
+	// ... 기존 스타일
+	editModeButton: {
+		position: 'absolute',
+		top: 10,
+		left: 10,
+	},
+	editButtonFamilyText: {
+
 	},
 });
