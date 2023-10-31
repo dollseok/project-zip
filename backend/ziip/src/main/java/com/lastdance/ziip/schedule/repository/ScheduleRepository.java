a266941740ba091b0a112e8545fc80670dc93265
choijehyuk <cjh970324@naver.com>
2023-10-31 16:38:03 +0900

    feat: 일자별 이슈 조회 플랜가져오기 구현완료 S09P31B206-126

--- backend/ziip/src/main/java/com/lastdance/ziip/schedule/repository/ScheduleRepository.java
+++ backend/ziip/src/main/java/com/lastdance/ziip/schedule/repository/ScheduleRepository.java
@@ -2,6 +2,8 @@ package com.lastdance.ziip.schedule.repository;
 
 import com.lastdance.ziip.family.repository.entity.Family;
 import com.lastdance.ziip.schedule.repository.entity.Schedule;
+
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,4 +12,6 @@ import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 public interface ScheduleRepository extends JpaRepository<Schedule, Long>, QuerydslPredicateExecutor<Schedule> {
 
     List<Schedule> findAllByFamily(Optional<Family> family);
+
+    List<Schedule> findAllByStartDate(LocalDate todayDateAsLocalDate);
 }
