ce08e77237cab8a6809496d395fffc2b83e9955e
이형석 <gudtjr2949@naver.com>
2023-11-12 03:19:32 +0900

    feat: 구글 AccessToken 리턴, Firebase 알림 전송

--- frontend/zipapp/screens/auth/KakaoLoginScreen.js
+++ frontend/zipapp/screens/auth/KakaoLoginScreen.js
@@ -4,16 +4,10 @@ import {StyleSheet, View, Text, Button} from 'react-native';
 import axios from 'axios';
 import {REST_API_KEY, REDIRECT_URI} from '@env';
 import firebase from '@react-native-firebase/app';
-// import * as firebase from '@react-native-firebase/app'
-import * as getMessaging from '@react-native-firebase/messaging';
-
-// import admin from 'firebase-admin';
-// import serviceAccount from '../../serviceAccountKey.json';
 
 const INJECTED_JAVASCRIPT = `window.ReactNativeWebView.postMessage('message from webView')`;
 
 export default function KakaoLoginScreen({navigation}) {
-
   const getCode = target => {
     const exp = 'code=';
     const condition = target.indexOf(exp);
@@ -25,14 +19,13 @@ export default function KakaoLoginScreen({navigation}) {
   };
 
   const requestToken = async code => {
-    // const requestTokenUrl = 'http://localhost:9090/api/members/kakao/login';
+    // const requestTokenUrl = 'http://10.0.2.2:9090/api/members/kakao/login';
     const requestTokenUrl = 'https://lastdance.kr/api/members/kakao/login';
 
     try {
       // FCM 토큰 가져오기 및 서버로 전송
       await firebase.messaging().registerDeviceForRemoteMessages();
       const fcmToken = await firebase.messaging().getToken();
-      await firebase.messaging().sendMessage()
 
       console.log('firebase 토큰 : ', fcmToken);
 
@@ -58,58 +51,31 @@ export default function KakaoLoginScreen({navigation}) {
         await AsyncStorage.setItem('refreshToken', refreshToken);
       }
 
-      console.log(response.data);
-
-      const admin = require('firebase-admin');
-
-      // admin.initializeApp({
-      //   credential: admin.credential.cert(serviceAccount)
-      // });
-
-      // const googleAccessToken = await admin.credential.applicationDefault().getAccessToken();
-
-      // console.log("구글에서 발급받은 토큰 : ", googleAccessToken);
-
-      // var serviceAccount = require("path/to/serviceAccountKey.json");
-
-      admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount)
-      });
-
+      const headers = {
+        Authorization: `Bearer ` + response.data.googleAccessToken,
+        'Content-Type': 'application/json'
+      };
 
       const message = {
-        'message': {
+        message: {
+          token:
+            '',
           notification: {
             title: '하진아 안녕',
             body: '하진아 안녕',
           },
-          tokens:
-            'cGLJ1LXkQ8Whr4vpOn_GRZ:cGLJ1LXkQ8Whr4vpOn_GRZ:APA91bFAKk2uG2QPrntVFLobIy_rQvBZxVCxm9DFMZe3RrIlbPMH3s7Y4T764uNr4EfMMa6VRmKBfBfElt46jtUB1mqdtiereKP0b2I7jQPd8ZDbVH8J7xeXsk5_rL6B2by9MAmbGCNw', // 여러 개의 토큰을 배열로 전달
         },
       };
-    
-      getMessaging().send(message).then(response => {
-            console.log('메시지 전송 성공 : ', response);
-          })
-          .catch(error => {
-            console.log('Firebase 전송 실패 : ', error);
-          });
 
-      // firebase
-      //   .messaging()
-      //   .sendMessage(message)
-      //   .then(response => {
-      //     console.log('메시지 전송 성공 : ', response);
-      //   })
-      //   .catch(error => {
-      //     console.log('Firebase 전송 실패 : ', error);
-      //   });
-      // try {
-      //   const firebaseResponse = await admin.messaging().sendMulticast(message);
-      //   console.log('Successfully sent message:', firebaseResponse);
-      // } catch (error) {
-      //   console.log('Error sending message:', error);
-      // }
+      const fcmUrl =
+        'https://fcm.googleapis.com/v1/projects/lastdance-test/messages:send';
+
+      await axios.post(fcmUrl, message, { headers: headers })
+      .then((response) => {
+        console.log("firebase 알림 전송 성공 : ", response)
+      }).catch((error) => {
+        console.log("firebase 알림 전송 실패 : ", error);
+      })
 
       await navigation.navigate('가족선택');
     } catch (e) {
