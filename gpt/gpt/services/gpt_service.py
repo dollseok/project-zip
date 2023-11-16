2803356b37f24a1874777a2f6b791faad407d6cb
hajin618 <1998rlagkwls@gmail.com>
2023-11-16 20:43:06 +0900

    feat: 스크립트 수정

--- gpt/gpt/services/gpt_service.py
+++ gpt/gpt/services/gpt_service.py
@@ -9,7 +9,7 @@ def get_gpt_message():
         model="gpt-3.5-turbo",
         messages=[
             {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": "가족과 함께 대화할 수 있는 주제 하나만 추천해줘. 주제만 말해줘. 한 문장으로 대답해줘. 여행 말고 다른 주제로 말해줘."},
+            {"role": "user", "content": "가족과 함께 대화할 수 있는 주제 하나만 추천해줘. 주제만 말해줘. 한 문장으로 대답해줘. 존댓말로 대답해줘"},
             {"role": "assistant", "content": "한국어로 대답해줘"}
         ]
     )
